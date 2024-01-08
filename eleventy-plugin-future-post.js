b40840ba36cd20cbff63f8d5d61bdc0e0a90ac15
John M. Wargo <johnwargo@users.noreply.github.com>
2024-01-08 08:35:23 -0500

    refactored

--- eleventy-plugin-future-post.js
+++ eleventy-plugin-future-post.js
@@ -2,17 +2,19 @@
 
 module.exports = function (eleventyConfig) {
 
+    var currentTime = new Date();
+
     eleventyConfig.addGlobalData("eleventyComputed.eleventyExcludeFromCollections", () => {
         return (data) => {
-            // get the post date
             var pageDate = new Date(data.page.date);
-            // if the post date is in the future, exclude it from collections
-            if (pageDate > new Date()) {
-                return true;
-            }
-            // otherwise, return what's already set for the post
-            return data.eleventyExcludeFromCollections;
+            return (pageDate > currentTime) ? true : data.eleventyExcludeFromCollections;
         }
     });
 }
 
+// if the post date is in the future, exclude it from collections
+// if (pageDate > new Date()) {
+//     return true;
+// }
+// // otherwise, return what's already set for the post
+// return data.eleventyExcludeFromCollections;
